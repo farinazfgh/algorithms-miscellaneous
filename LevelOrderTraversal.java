678bbc561f0f95b63705f5dae4daad77380fb266
Farinaz <farinaz.ghasemi@gmail.com>
2020-12-30 17:07:12 +0100

    gives all nodes at a certain level of the tree2

--- LevelOrderTraversal.java
+++ LevelOrderTraversal.java
@@ -1,4 +1,98 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
-public class LevelOrderTraversal {
+/*
+    at every iteration queue size indicates the level:
+    queue size: 1 => we are at level 1
+    queue size: 2 => we are at level 2
+    etc.
+ */
+class LevelOrderTraversal {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static List<List<Integer>> traverse(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                Node currentNode = queue.poll();
+                currentLevel.add(currentNode.data);
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
+            }
+            result.add(currentLevel);
+        }
+
+        return result;
+    }
+
+    public static List<List<Integer>> traverseReverseLeve(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                Node currentNode = queue.poll();
+                currentLevel.add(currentNode.data);
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
+            }
+            result.add(0, currentLevel);
+        }
+
+        return result;
+    }
+
+    public static void bfs(Node root) {
+        if (root == null)
+            return;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            if (currentNode.left != null)
+                queue.offer(currentNode.left);
+            if (currentNode.right != null)
+                queue.offer(currentNode.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(12);
+        root.left = new Node(7);
+        root.right = new Node(1);
+        root.left.left = new Node(9);
+        root.right.left = new Node(10);
+        root.right.right = new Node(5);
+        List<List<Integer>> result = LevelOrderTraversal.traverse(root);
+        System.out.println("Level order traversal: " + result);
+        List<List<Integer>> resultReverse = LevelOrderTraversal.traverseReverseLeve(root);
+        System.out.println("Level order traversal reverse: " + resultReverse);
+    }
 }
